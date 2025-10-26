62cff8cb4e091199e4425c4bf39ecd9af4e09306
Ilya Koshman <radirmail044@gmail.com>
2025-10-26 17:53:49 +0300

    update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Jewelery
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            TimeBox.Content = DateTime.UtcNow;
         }
 
         private void ThemeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) =>
@@ -30,9 +18,6 @@ namespace Jewelery
            (SolidColorBrush)Application.Current.Resources["LightTheme"] :
            (SolidColorBrush)Application.Current.Resources["DarkTheme"];
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(new DataPage("Вкладка"));
-        }
+        private void Button_Click(object sender, RoutedEventArgs e) => Frame.Navigate(new DataPage(((((Button)sender).Content as Grid).Children[1] as TextBlock).Text, ((Button)sender).Tag.ToString(), ((Button)sender).Uid, $"Select * From {((Button)sender).Tag.ToString()}_view"));
     }
 }
